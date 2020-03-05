39df1d8d4683c73c9663a0127654ee2f97260da2
Bonani Brian Wogqoyi <bbwogqoyi@outlook.com>
2020-03-05 16:46:05 +0200

    Completed Task 1 - Task 9

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -1,34 +1,62 @@
 ﻿module Synthesis
 
-let abelar _ =
-    failwith "Not implemented"
+let abelar num =
+  let isWithInRange = num > 12 && num < 3097
+  let isDivisibleBy12 = (num % 12 = 0)
+  isWithInRange && isDivisibleBy12
 
-let area _ _ =
-    failwith "Not implemented"
+let area baseLength height =
+  match baseLength>=0.0 && height>=0.0 with
+  | true -> 0.5*baseLength*height
+  | _ -> failwith "Negative values not allowed"
 
-let zollo _ =
-    failwith "Not implemented"
+let zollo num =
+  match num>=0 with
+  | true -> num*2
+  | false -> -1*num
 
-let min _ _ =
-    failwith "Not implemented"
+let min a b =
+  match a>b with
+  | true -> b
+  | _ -> a
 
-let max _ _ =
-    failwith "Not implemented"
+let max a b =
+  match a<b with
+  | true -> b
+  | _ -> a
 
-let ofTime _ _ _ =
-    failwith "Not implemented"
+let ofTime hours minutes seconds =
+  hours*60*60 + minutes*60 + seconds
 
-let toTime _ =
-    failwith "Not implemented"
+let toTime totalTime =
+  match totalTime>=0 with
+  | false -> (0,0,0)
+  | _ ->
+    let hours = totalTime / 3600
+    let minutes = (totalTime - hours*3600) / 60
+    let seconds = totalTime - (hours*3600 + minutes*60)
+    (hours, minutes, seconds)
+  
+let digits num =
+  let rec countDigits count rem =
+    match rem <> 0, count with
+    | true, _ -> countDigits (count+1) (rem/10)
+    | _, 0 -> count+1
+    | _ -> count
+  countDigits 0 num
 
-let digits _ =
-    failwith "Not implemented"
+let minmax (a,b,c,d) =
+  let rec helper (smallest, largest) alist =
+    match alist with 
+    | [] -> (smallest, largest)
+    | head::tail -> helper ( (min smallest head), (max largest head) ) tail
+  helper (a,a) ([a;b;c;d])
 
-let minmax _ =
-    failwith "Not implemented"
-
-let isLeap _ =
-    failwith "Not implemented"
+let isLeap year =
+  match year<1582 with
+  | true -> failwith "Value of the Year is not allowed to be less than 1582"
+  | _ -> 
+    match year%4 = 0 && year%400<>0 && year 
 
 let month _ =
     failwith "Not implemented"
